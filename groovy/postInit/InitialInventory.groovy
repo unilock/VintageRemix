9d5c337c29b801995f27a7cd0697fe20cd9a4baa
unilock <unilock@fennet.rentals>
2024-10-14 15:11:10 -0400

    init groovy + incl lwjgl3ify jar
    Signed-off-by: unilock <unilock@fennet.rentals>

---
+++ groovy/postInit/InitialInventory.groovy
@@ -0,0 +1,5 @@
+// InitialInventory
+player.testingStartingItems = false // Enable this to have the items be given every time you join the world. Use in testing only.
+player.replaceDefaultInventory = false // Enable this to replace any existing items with GroovyScript's starting inventory items.
+
+player.addStartingItem(item('akashictome:tome').withNbt(null))
