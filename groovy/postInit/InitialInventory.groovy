292cc128e07a115824021754ea2405ee2471cb3b
unilock <unilock@fennet.rentals>
2024-10-14 16:06:36 -0400

    finish akashic tome on first login
    Signed-off-by: unilock <unilock@fennet.rentals>

--- groovy/postInit/InitialInventory.groovy
+++ groovy/postInit/InitialInventory.groovy
@@ -1,5 +1,232 @@
 // InitialInventory
+
+import net.minecraftforge.event.entity.player.ItemTooltipEvent
+
 player.testingStartingItems = false // Enable this to have the items be given every time you join the world. Use in testing only.
 player.replaceDefaultInventory = false // Enable this to replace any existing items with GroovyScript's starting inventory items.
 
-player.addStartingItem(item('akashictome:tome').withNbt(null))
+player.addStartingItem(item('minecraft:stone_sword'))
+player.addStartingItem(item('minecraft:stone_pickaxe'))
+player.addStartingItem(item('minecraft:stone_axe'))
+player.addStartingItem(item('minecraft:stone_shovel'))
+
+player.addStartingItem(item('akashictome:tome').withNbt([
+	'akashictome:data': [
+		'arcanearchives': [
+			'Count': 1 as byte,
+			'Damage': 0 as short,
+			'id': 'arcanearchives:tome_arcana',
+			'tag': [
+				'akashictome:definedMod': 'arcanearchives',
+				'Book': 'arcanearchives:xml/tome.xml'
+			]
+		],
+		'bloodarsenal': [
+			'Count': 1 as byte,
+			'Damage': 0 as short,
+			'id': 'guideapi:bloodarsenal-guide',
+			'tag': [
+				'akashictome:definedMod': 'bloodarsenal'
+			]
+		],
+		'buildcraftlib': [
+			'Count': 1 as byte,
+			'Damage': 0 as short,
+			'id': 'buildcraftlib:guide',
+			'tag': [
+				'akashictome:definedMod': 'buildcraftlib'
+			]
+		],
+		'correlated': [
+			'Count': 1 as byte,
+			'Damage': 0 as short,
+			'id': 'correlated:doc_tablet',
+			'tag': [
+				'akashictome:definedMod': 'correlated'
+			]
+		],
+		'corvus': [
+			'Count': 1 as byte,
+			'Damage': 0 as short,
+			'id': 'patchouli:guide_book',
+			'tag': [
+				'akashictome:definedMod': 'corvus',
+				'patchouli:book': 'corvus:book'
+			]
+		],
+		'elementaristics': [
+			'Count': 1 as byte,
+			'Damage': 0 as short,
+			'id': 'patchouli:guide_book',
+			'tag': [
+				'akashictome:definedMod': 'elementaristics',
+				'patchouli:book': 'elementaristics:liber_elementium'
+			]
+		],
+		'embers': [
+			'Count': 1 as byte,
+			'Damage': 0 as short,
+			'id': 'embers:codex',
+			'tag': [
+				'akashictome:definedMod': 'embers'
+			]
+		],
+		'engineersdecor': [
+			'Count': 1 as byte,
+			'Damage': 0 as short,
+			'id': 'patchouli:guide_book',
+			'tag': [
+				'akashictome:definedMod': 'engineersdecor',
+				'patchouli:book': 'engineersdecor:engineersdecor_manual'
+			]
+		],
+		'extrautils2': [
+			'Count': 1 as byte,
+			'Damage': 0 as short,
+			'id': 'extrautils2:book',
+			'tag': [
+				'akashictome:definedMod': 'extrautils2'
+			]
+		],
+		'mahoutsukai': [
+			'Count': 1 as byte,
+			'Damage': 0 as short,
+			'id': 'mahoutsukai:guidebook',
+			'tag': [
+				'akashictome:definedMod': 'mahoutsukai'
+			]
+		],
+		'naturesaura': [
+			'Count': 1 as byte,
+			'Damage': 0 as short,
+			'id': 'patchouli:guide_book',
+			'tag': [
+				'akashictome:definedMod': 'naturesaura',
+				'patchouli:book': 'naturesaura:book'
+			]
+		],
+		'opencomputers': [
+			'Count': 1 as byte,
+			'Damage': 4 as short,
+			'id': 'opencomputers:tool',
+			'tag': [
+				'akashictome:definedMod': 'opencomputers'
+			]
+		],
+		'practicallogistics2': [
+			'Count': 1 as byte,
+			'Damage': 0 as short,
+			'id': 'practicallogistics2:plguide',
+			'tag': [
+				'akashictome:definedMod': 'practicallogistics2'
+			]
+		],
+		'quiverbow_restrung': [
+			'Count': 1 as byte,
+			'Damage': 0 as short,
+			'id': 'patchouli:guide_book',
+			'tag': [
+				'akashictome:definedMod': 'quiverbow_restrung',
+				'patchouli:book': 'quiverbow_restrung:weaponsmiths_journal'
+			]
+		],
+		'rftools': [
+			'Count': 1 as byte,
+			'Damage': 0 as short,
+			'id': 'rftools:rftools_manual',
+			'tag': [
+				'akashictome:definedMod': 'rftools'
+			]
+		],
+		'rftools1': [
+			'Count': 1 as byte,
+			'Damage': 0 as short,
+			'id': 'rftools:rftools_shape_manual',
+			'tag': [
+				'akashictome:definedMod': 'rftools1'
+			]
+		],
+		'rftools2': [
+			'Count': 1 as byte,
+			'Damage': 0 as short,
+			'id': 'rftoolsdim:rftoolsdim_manual',
+			'tag': [
+				'akashictome:definedMod': 'rftools2'
+			]
+		],
+		'roots': [
+			'Count': 1 as byte,
+			'Damage': 0 as short,
+			'id': 'roots:roots_guide',
+			'tag': [
+				'akashictome:definedMod': 'roots'
+			]
+		],
+		'theaurorian': [
+			'Count': 1 as byte,
+			'Damage': 0 as short,
+			'id': 'patchouli:guide_book',
+			'tag': [
+				'akashictome:definedMod': 'theaurorian',
+				'patchouli:book': 'theaurorian:the_aurorian_guide'
+			]
+		],
+		'thebetweenlands': [
+			'Count': 1 as byte,
+			'Damage': 0 as short,
+			'id': 'thebetweenlands:manual_hl',
+			'tag': [
+				'akashictome:definedMod': 'thebetweenlands'
+			]
+		],
+		'tis3d': [
+			'Count': 1 as byte,
+			'Damage': 0 as short,
+			'id': 'tis3d:book_manual',
+			'tag': [
+				'akashictome:definedMod': 'tis3d'
+			]
+		],
+		'twilightforest': [
+			'Count': 1 as byte,
+			'Damage': 0 as short,
+			'id': 'patchouli:guide_book',
+			'tag': [
+				'akashictome:definedMod': 'twilightforest',
+				'patchouli:book': 'twilightforest:guide'
+			]
+		],
+		'woot': [
+			'Count': 1 as byte,
+			'Damage': 0 as short,
+			'id': 'guideapi:woot-guide',
+			'tag': [
+				'akashictome:definedMod': 'woot'
+			]
+		],
+		'xnet': [
+			'Count': 1 as byte,
+			'Damage': 0 as short,
+			'id': 'xnet:xnet_manual',
+			'tag': [
+				'akashictome:definedMod': 'xnet'
+			]
+		]
+	]
+]))
+
+player.addStartingItem(item('astralsorcery:itemjournal'))
+player.addStartingItem(item('alquimia:lexicon'))
+player.addStartingItem(item('botania:lexicon'))
+player.addStartingItem(item('levelup2:skillbook'))
+
+player.addStartingItem(item('minecraft:leather_helmet'))
+player.addStartingItem(item('minecraft:leather_chestplate'))
+player.addStartingItem(item('minecraft:leather_leggings'))
+player.addStartingItem(item('minecraft:leather_boots'))
+
+event_manager.listen { ItemTooltipEvent event ->
+    if (event.getItemStack() in item('akashictome:tome')) {
+        event.getToolTip() << '§cDO NOT ADD: Astral Tome, Lexica Alquimia, Lexica Botania, Skill Book'
+    }
+}
